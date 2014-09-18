8722d203fc3246ae4b91ae7fb28136ea21ebca51
stanley zheng <dev.stanzheng@gmail.com>
2014-09-18 16:09:18 -0400

    creating backbone collections and view

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var Backbone = require('backbone');
 var $ = require('jquery');
 Backbone.$ = $;
+/**
+Miners
+**/
 var Miner = require('./src/js/Miner.js');
+var MinerCollection = require('./src/js/collections/MinerCollection.js');
+var MinerView = require('./src/js/views/MinerView.js');
 
-console.log(Miner);
-var person = Backbone.Collection.extend({
-  model: Miner
-});
+console.log(MinerCollection);
+var Servers = new MinerCollection.Miners();
+console.log(Servers);
