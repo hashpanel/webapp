f1ebf407e51a769804612e1c81010f0ea13abffc
stanley zheng <dev.stanzheng@gmail.com>
2014-09-18 13:09:19 -0400

    restructing app

---
+++ app.js
@@ -0,0 +1,7 @@
+var $ = require('jquery')(window);
+var Backbone = require('backbone');
+Backbone.$ = $;
+
+var x,y,z;
+x = "foo";
+console.log(x);
--- src/js/app.js
+++
@@ -1,3 +0,0 @@
-var x,y,z;
-x = "foo";
-console.log(x);
