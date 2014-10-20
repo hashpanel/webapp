84ba682b2db1dd4a9f7b562a5675c99ee6a52df0
stanley zheng <dev.stanzheng@gmail.com>
2014-10-20 17:02:34 -0400

    updating model to match one in hashware api

--- src/js/Miner.js
+++ src/js/Miner.js
@@ -3,16 +3,72 @@ var Backbone = require('backbone');
 module.exports = Backbone.Model.extend({
    urlRoot: '/miner',
    defaults: {
-        'id': '',
-        'name': '',
-        'model': '',
-        'hashRate': '',
-        'powerUsage': '',
-        'purchasePrice': '',
-        'beginService': '',
-        'endService': '',
-        'connection': '',
-        'site': '',
-        'group': ''
+    name: {
+      type: 'string',
+      notNull: true
+    },
+    device: {
+      model: 'MinerDevice'
+    },
+    notes: {
+      type: 'text'
+    },
+    hashRate: {
+      // declared hashrate in GH/s
+      type: 'integer'
+    },
+    powerUsage: {
+      // declared power consumption in Watts
+      type: 'integer'
+    },
+    purchasePrice: {
+      type: 'float'
+    },
+    monthlyFee: {
+      type: 'float'
+    },
+    beginService: {
+      type: 'date'
+    },
+    endService: {
+      type: 'date'
+    },
+    host: {
+      type: 'string'
+    },
+    port: {
+      type: 'integer',
+      defaultsTo: 4028
+    },
+    internalAddress: {
+      type: 'string'
+    },
+    site: {
+      model: 'Site',
+      index: true
+    },
+    group: {
+      model: 'Group',
+      index: true
+    },
+    owner: {
+      model: 'User',
+      index: true
+    },
+    state: {
+      model: 'MinerState'
+    },
+    history: {
+      collection: 'MinerState',
+      via: 'miner'
+    },
+    interval: {
+      type: 'integer',
+      defaultsTo: 300
+    },
+    worker: {
+      collection: 'PoolWorker',
+      via: 'miner'
+    }
   }
 });
