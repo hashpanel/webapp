77b1ff42b542f9f3c90b4a3bec1822ded2da11d6
stanley zheng <dev.stanzheng@gmail.com>
2014-10-21 00:32:15 -0400

    swapping in hashware api live

--- src/js/collections/MinerCollection.js
+++ src/js/collections/MinerCollection.js
@@ -4,6 +4,7 @@ Backbone.LocalStorage = require("backbone.localstorage");
 
 module.exports = Backbone.Collection.extend({
   model: Miner,
-  url: "http://localhost:1337/api/v1/miner",
+  //url: "http://localhost:1337/api/v1/miner",
+  url: "http://hashware-api.herokuapp.com/api/v1/miner"
 //  localStorage: new Backbone.LocalStorage("miners-backbone"),
 });
