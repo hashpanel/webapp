7660c6921b16a876e9ddeaf0a3475a417d74b9af
Travis Webb <me@traviswebb.com>
2014-12-06 03:33:30 -0500

    add bower; refactor gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
-var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
+var source = require('vinyl-source-stream');
+var transform = require('vinyl-transform');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify'); 
@@ -7,26 +8,24 @@ var concat = require('gulp-concat');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
 var plumber = require('gulp-plumber');
+var bower = require('gulp-bower');
+var uglify = require('gulp-uglify');
  
 gulp.task('browserify', function() {
-  var bundler = browserify({
-    entries: ['./src/index.js'],
-    transform: [reactify],
-    debug: true,
-    cache: { }, packageCache: { }, fullPaths: true
+  var browserified = transform(function (filename) {
+    var b = browserify(filename);
+    b.transform(reactify);
+    return b.bundle();
   });
-  var watcher = watchify(bundler);
 
-  return watcher.on('update', function () {
-      var t0 = Date.now();
-      watcher.bundle()
-        .pipe(source('app.js'))
-        .pipe(gulp.dest('./www/'));
-
-      console.log('updated build. time:', (Date.now() - t0) + 'ms');
-    })
-    .bundle()
-    .pipe(source('app.js'))
+  return gulp.src([
+      './src/index.js',
+      './bower_components/d3/d3.js',
+      './bower_components/nvd3/nv.d3.js'
+    ])
+    .pipe(browserified)
+    .pipe(concat('hashpanel.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('./www/'));
 });
 
@@ -34,15 +33,19 @@ gulp.task('less', function () {
   return gulp.src('./src/styles/index.less')
     .pipe(plumber())
     .pipe(less())
+    .pipe(concat('hashpanel.css'))
     .pipe(gulp.dest('./www/'));
 });
 
 gulp.task('watch', function () {
   gulp.watch([
-      './src/styles/*.less'
+      '*.less',
+      '*.js',
+      '*.jsx'
     ],
-    [ 'less' ]
+    { base: './src' },
+    [ 'less', 'browserify' ]
     );
 });
 
-gulp.task('default', ['browserify', 'less', 'watch']);
+gulp.task('default', ['browserify', 'less']);
