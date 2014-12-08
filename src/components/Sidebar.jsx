0ea3f06427af3166a2c743f4a0533cba52d3f96f
Travis Webb <me@traviswebb.com>
2014-12-08 14:32:40 -0500

    fixed icon formatting

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,25 +9,25 @@ var Sidebar = React.createClass({
       <bs.ListGroup>
         <Link to='dashboard'>
           <bs.ListGroupItem active>
-            <bs.Glyphicon className='fa-dashboard fa-fw' />
+            <span className='fa-dashboard fa-fw' />
             Dashboard
           </bs.ListGroupItem>
         </Link>
         <Link to='miners'>
           <bs.ListGroupItem>
-            <bs.Glyphicon className='fa-hdd-o fa-fw' />
+            <span className='fa-hdd-o fa-fw' />
             Miners
           </bs.ListGroupItem>
         </Link>
         <Link to='analysis'>
           <bs.ListGroupItem>
-            <bs.Glyphicon className='fa-line-chart fa-fw' />
+            <span className='fa-line-chart fa-fw' />
             Analysis
           </bs.ListGroupItem>
         </Link>
         <Link to='news'>
           <bs.ListGroupItem>
-            <bs.Glyphicon className='fa-newspaper-o fa-fw' />
+            <span className='fa-newspaper-o fa-fw' />
             News
           </bs.ListGroupItem>
         </Link>
