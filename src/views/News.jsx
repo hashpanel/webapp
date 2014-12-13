660f2742f650398abe7e8efb18649d29e66effbf
Travis Webb <me@traviswebb.com>
2014-12-13 00:56:45 -0500

    implemented news feed

--- src/views/News.jsx
+++ src/views/News.jsx
@@ -1,39 +1,45 @@
 /** @jsx React.DOM */
 
 var bs = React.Bootstrap;
+var moment = require('moment');
 
 var News = React.createClass({
   getInitialState: function () {
     return {
-      rss: 'https://newsbtc.com/feed/',
+      rss: 'http://localhost:1337/api/v1/news',
       items: { }
     };
   },
   renderNewsItem: function (item) {
+    console.log(item);
     return (
-      <bs.ListGroupItem header={item.title} href={item.url} hover>
+      <bs.ListGroupItem header={item.title} href={item.url} target='_blank' hover>
         <p class='newsitem-date'>Published: {item.date}</p>
         <p class='newsitem-summary'>{item.summary}</p>
       </bs.ListGroupItem>
     );
   },
-  componentDidMount: function () {
+  componentWillMount: function () {
+    var self = this;
     $.get(this.state.rss, function (data) {
       $(data).find('item').each(function () {
         var item = $(this);
+        var url = item.find('link').text();
 
-        var items = { };
+        var items = self.state.items;
         items[url] = {
           title: item.find('title').text(),
-          url: item.find('link').text(),
-          summary: item.find('description').text(),
-          date: new Date(item.find('pubDate').text())
+          url: url,
+          summary: item.find('description').text().replace(/<(?:.|\n)*?>/gm, '').split('.')[0] + '.',
+          date: moment(new Date(item.find('pubDate').text())).format('dddd, MMMM Do YYYY')
         };
-        this.setState({ items: items });
+        console.log(items[url]);
+        self.setState({ items: items });
       });
     });
   },
   render: function () {
+    console.log(this.state);
     return (
       <bs.Panel header='News'>
         <bs.ListGroup>
