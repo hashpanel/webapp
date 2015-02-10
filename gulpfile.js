3907b7f87fc27e9e7fe9513757edfbae2c98f95b
Travis Webb <me@traviswebb.com>
2015-02-10 15:00:49 -0500

    new build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var transform = require('vinyl-transform');
@@ -10,6 +11,23 @@ var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
 var bower = require('main-bower-files');
 var buffer = require('vinyl-buffer');
+var logger = require('gulp-logger');
+var s3 = require('gulp-s3');
+
+var bundler = browserify({
+  entries: [
+    './js/index.js'
+  ],
+  transform: [ reactify ],
+  debug: true,
+  cache: { },
+  packageCache: { },
+  fullPaths: true
+});
+
+var config = {
+  bowerDir: './bower_components'
+};
  
 gulp.task('browserify-lib', function() {
   return browserify({
@@ -30,45 +48,62 @@ gulp.task('browserify-lib', function() {
 });
 
 gulp.task('browserify-app', function () {
-  var bundler = browserify({
-    entries: [
-      './src/index.js'
-    ],
-    transform: [ reactify ],
-    debug: true,
-    cache: { },
-    packageCache: { },
-    fullPaths: true
-  });
+  return bundler
+    .bundle()
+    .pipe(source('hashpanel.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./www/'));
+});
 
+gulp.task('browserify-app-watch', function () {
   var watcher = watchify(bundler);
     
   return watcher.on('update', function () {
-    var t0 = Date.now();
+      var t0 = Date.now();
       watcher.bundle()
         .pipe(source('hashpanel.js'))
         .pipe(buffer())
-        //.pipe(uglify())
+        .pipe(logger({
+          before: 'Change detected, recompiling...',
+          after: 'Complete'
+        }))
         .pipe(gulp.dest('./www/'));
-
-      console.log('js updated;', (Date.now() - t0) + 'ms');
     })
     .bundle()
     .pipe(source('hashpanel.js'))
     .pipe(buffer())
-    //.pipe(uglify())
     .pipe(gulp.dest('./www/'));
 });
 
 gulp.task('less', function () {
-  return gulp.src('./src/styles/index.less')
+  return gulp.src('./css/index.less')
     .pipe(less())
     .pipe(concat('hashpanel.css'))
     .pipe(gulp.dest('./www/'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch([ 'src/styles/*.less' ], [ 'less' ]);
+gulp.task('less-watch', function () {
+  gulp.watch([ 'css/*.less' ], [ 'less' ]);
 });
 
-gulp.task('default', ['browserify-lib', 'browserify-app', 'less', 'watch']);
+gulp.task('s3-deploy', function () {
+  gulp.src('www/**')
+    .pipe(s3({
+      key: process.env.AWS_ACCESSID,
+      secret: process.env.AWS_SECRETKEY,
+      bucket: 'www.hashpanel.io'
+    }));
+});
+
+gulp.task('icons', function () {
+  gulp.src([
+      './node_modules/font-awesome/fonts/**.*',
+      './node_modules/bootstrap/fonts/**.*'
+    ])
+    .pipe(gulp.dest('www/fonts'));
+});
+
+gulp.task('watch', ['browserify-lib', 'icons', 'browserify-app', 'browserify-app-watch', 'less', 'less-watch']);
+gulp.task('build', ['browserify-lib', 'icons', 'browserify-app', 'less' ]);
+gulp.task('deploy', [ 'build', 's3-deploy' ]);
